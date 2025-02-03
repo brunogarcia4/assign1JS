1ce8613d218af7a983a9e43c11b896221f15a901
brunogarcia4 <114751082+brunogarcia4@users.noreply.github.com>
2025-02-03 14:33:55 -0500

    Update readFile.js

--- Assign1/readFile.js
+++ Assign1/readFile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('textfile.txt', 'utf8', (err, data) => {
+fs.readFile('crypto.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Error to read the file:', err);
         return;
